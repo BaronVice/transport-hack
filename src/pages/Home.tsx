ff5c71b76b2959789529f538a4772f821d450947
Pavel Naumov <malinatrashh@gmail.com>
2024-05-21 15:11:21 +0800

    mock

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button'
 
 const Home = () => {
 	return (
-		<div className='flex h-screen bg-blue-50 justify-between items-center select-none'>
+		<div className='flex h-screen justify-between items-center select-none'>
 			<Sidebar/>
 			<Button>FFFF</Button>
 		</div>
