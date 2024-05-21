008ad22c74ee11256dcbc8394d9ec0403f4a4368
TShuri <andr.xti@gmail.com>
2024-05-21 15:17:12 +0800

    Сделано отображение карты
    Карта открывается с начальными координатами города

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,12 @@
 import Sidebar from '@/components/SideBar'
 import { Button } from '@/components/ui/button'
+import MapboxMap from '@/components/Map'
 
 const Home = () => {
 	return (
 		<div className='flex h-screen bg-blue-50 justify-between items-center select-none'>
 			<Sidebar/>
-			<Button>FFFF</Button>
+			<MapboxMap></MapboxMap>
 		</div>
 	);
 };
